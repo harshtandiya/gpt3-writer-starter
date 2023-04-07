0fd267fa480723ee1a658c3985b636b54e6a3249
Harsh Tandiya <harsh.tandiya@gmail.com>
2023-04-08 02:14:44 +0530

    hooked OpenAI API to backend

---
+++ pages/api/generate.js
@@ -0,0 +1,27 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix = `You are Naruto from the Naruto and Naruto:Shippuden series. Act and reply as the character of Naruto. Use catchphrase "Dattebayo!" wherever appropriate but not always.
+`;
+
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.9,
+    max_tokens: 400,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,34 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
-import narutoGif from '../assets/naruto-gif.gif';
+import narutoGif from '../assets/naruto-gif2.gif';
 import { useState } from 'react';
 
 const Home = () => {
   const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+
   const onUserChangedText = (e) => {
     setUserInput(e.target.value);
   };
@@ -19,6 +42,7 @@ const Home = () => {
           <div className="header-title">
             <h1>talk to Naruto about anything</h1>
             <Image className='naruto' src={narutoGif} alt="Naruto GIF"/>
+            {/* <iframe src="https://giphy.com/embed/kDRYLFBNt48zm" width="160" height="200" frameBorder="0" class="giphy-embed" allowFullScreen></iframe> */}
           </div>
           <div className="header-subtitle">
             <p>write a message to Naruto from the Naruto shonen series, and ask him about anything.(ex: what is friendship?, what is life supposed to be?)</p>
@@ -30,13 +54,40 @@ const Home = () => {
           <textarea placeholder="start typing here" className="prompt-box" value={userInput} onChange={onUserChangedText}/>
         </div> 
       </div>
-      <div className="promt-container">
-        <a className="generate-button" onClick={null}>
+      {/* <div className="promt-container">
+        <a className="generate-button" onClick={callGenerateEndpoint}>
           <div className="generate">
             <p>Generate</p>
           </div>
         </a>      
+      </div> */}
+      <div className="prompt-container">
+        <div className="prompt-buttons">
+          <a
+            className={isGenerating ? 'generate-button loading' : 'generate-button'}
+            onClick={callGenerateEndpoint}
+          >
+            <div className="generate">
+            {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+            </div>
+          </a>
+        </div>
       </div>
+
+      {apiOutput && (
+        <div className="output">
+          <div className="output-header-container">
+            <div className="output-header">
+              <h3>Output</h3>
+            </div>
+          </div>
+          <div className="output-content">
+            <p>{apiOutput}</p>
+          </div>
+        </div>
+      )}
+      
+
       <div className="badge-container grow">
         <a
           href="https://buildspace.so/builds/ai-writer"
